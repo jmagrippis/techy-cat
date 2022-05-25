183e4c737368407fd9e1a224365266793de1a6dd
Johnny Magrippis <j@magrippis.com>
2022-05-25 20:03:28 +0200

    ✨  add /ideas/[id] endpoint

---
+++ src/routes/ideas/[id].ts
@@ -0,0 +1,16 @@
+import {ideasRepo} from '$lib/repos/ideas'
+import type {RequestHandler} from './__types/[id]'
+
+export const get: RequestHandler = async ({params}) => {
+	const idea = await ideasRepo.find(params.id)
+
+	if (idea) {
+		return {
+			body: {idea},
+		}
+	}
+
+	return {
+		status: 404,
+	}
+}
