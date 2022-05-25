ba8348157228feb4e0682bd428d1b2f186a1aedc
Johnny Magrippis <j@magrippis.com>
2022-05-25 20:03:28 +0200

    ✨  add /ideas/[id] endpoint

--- src/lib/repos/ideas.ts
+++ src/lib/repos/ideas.ts
@@ -1,8 +1,8 @@
 import {Client} from '@notionhq/client'
 import {getServerOnlyEnvVar} from '$lib/getServerOnlyEnvVar'
-import type {QueryDatabaseResponse} from '@notionhq/client/build/src/api-endpoints'
 
 export type Idea = {
+	id: string
 	name: string
 	emoji: string
 	description: string
@@ -11,6 +11,7 @@ export type Idea = {
 const IDEAS_DB_ID = '864d832403c54dc1b062376efa807f18'
 
 type NotionIdeaResult = {
+	id: string
 	icon: {
 		emoji: string
 	}
@@ -43,29 +44,38 @@ const isNotionResult = (
 		(result as NotionIdeaResult)?.properties.Description.rich_text
 	)
 
-const mapResultToIdea = (notionResult: Record<string, unknown>): Idea | null =>
-	isNotionResult(notionResult)
-		? {
-				name: notionResult.properties.Name.title[0].text.content,
-				emoji: notionResult.icon.emoji,
-				description: notionResult.properties.Description.rich_text
-					.reduce((acc, {text: {content}}) => `${acc} ${content}`, '')
-					.trim(),
-		  }
-		: null
+const mapResultToIdea = (notionResult: NotionIdeaResult): Idea => ({
+	id: notionResult.id,
+	name: notionResult.properties.Name.title[0].text.content,
+	emoji: notionResult.icon.emoji,
+	description: notionResult.properties.Description.rich_text
+		.reduce((acc, {text: {content}}) => `${acc} ${content}`, '')
+		.trim(),
+})
 
 class IdeasRepo {
 	#client = new Client({
 		auth: getServerOnlyEnvVar('NOTION_TOKEN'),
 	})
 
-	getAll = async ({limit}: {limit: number}) => {
+	getAll = async ({limit}: {limit: number}): Promise<Idea[]> => {
 		const {results} = await this.#client.databases.query({
 			database_id: IDEAS_DB_ID,
 			page_size: limit,
 		})
 
-		return results.map(mapResultToIdea).filter(Boolean)
+		return results.reduce<Idea[]>((acc, result) => {
+			if (isNotionResult(result)) {
+				acc.push(mapResultToIdea(result))
+			}
+			return acc
+		}, [])
+	}
+
+	find = async (id: string): Promise<Idea | null> => {
+		const result = await this.#client.pages.retrieve({page_id: id})
+
+		return isNotionResult(result) ? mapResultToIdea(result) : null
 	}
 }
 
