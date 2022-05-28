bdc3aab48bfa486174ba4ee723c3d2ccdc097acb
Johnny Magrippis <j@magrippis.com>
2022-05-28 22:04:43 +0200

    ♻️  slug -> ideas and supabase client improvements

--- src/routes/ideas/[slug].ts
+++ src/routes/ideas/[slug].ts
@@ -1,8 +1,8 @@
 import {ideasRepo} from '$lib/repos/ideas'
-import type {RequestHandler} from './__types/[id]'
+import type {RequestHandler} from './__types/[slug]'
 
 export const get: RequestHandler = async ({params}) => {
-	const idea = await ideasRepo.find(params.id)
+	const idea = await ideasRepo.find(params.slug)
 
 	if (!idea) {
 		return {
