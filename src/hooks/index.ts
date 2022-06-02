4ad51d2dd6b83f97651b34d5889a9477214c0ac3
Johnny Magrippis <j@magrippis.com>
2022-06-02 11:47:15 +0200

    🔥 nuke code to reimpliment for recording

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,5 @@
 import type {GetSession, Handle} from '@sveltejs/kit'
-import {createClient} from '@supabase/supabase-js'
 import {getCookieValue} from '$lib/getCookieValue'
-import {IdeasRepo} from '$lib/repos/ideas'
 
 import {isTheme, type Theme} from '../types'
 
@@ -10,17 +8,10 @@ const getThemeFromCookie = (cookie: string | null): Theme => {
 	return isTheme(theme) ? theme : 'auto'
 }
 
-const supabaseClient = createClient(
-	import.meta.env.VITE_SUPABASE_URL,
-	import.meta.env.VITE_SUPABASE_ANON_KEY
-)
-const ideasRepo = new IdeasRepo(supabaseClient)
-
 export const handle: Handle = async ({event, resolve}) => {
 	const cookie = event.request.headers.get('cookie')
 
 	event.locals.theme = getThemeFromCookie(cookie)
-	event.locals.ideasRepo = ideasRepo
 
 	return resolve(event)
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,5 @@
-<script context="module" lang="ts">
-	import type {Load} from '@sveltejs/kit'
-
-	export const load: Load = async ({fetch}) => {
-		const url = '/ideas?limit=3'
-		const headers = new Headers({
-			Accept: 'application/json',
-		})
-
-		const response = await fetch(url, {headers})
-
-		if (!response.ok) {
-			return {status: response.status}
-		}
-
-		const {ideas} = await response.json()
-
-		return {
-			props: {ideas},
-			cache: {
-				maxage: 1 * 60,
-			},
-		}
-	}
-</script>
-
 <script lang="ts">
-	import BigLink from '$lib/components/buttons/BigLink.svelte'
-
 	import Hero from '$lib/components/Hero/Hero.svelte'
-	import IdeaCard from '$lib/components/IdeaCard.svelte'
-	import type {IdeaSnippet} from '$lib/repos/ideas'
-
-	export let ideas: IdeaSnippet[]
 </script>
 
 <svelte:head>
@@ -39,18 +7,3 @@
 </svelte:head>
 
 <Hero />
-<section class="container mb-6 px-2 sm:px-0">
-	<h2 class="mb-4 text-2xl">Latest ideas</h2>
-	<ul
-		class="mb-6 flex max-w-prose flex-col gap-4 sm:grid sm:w-full sm:max-w-full sm:grid-cols-12"
-	>
-		{#each ideas as { emoji, name, description }}
-			<li class="sm:col-span-6 xl:col-span-4">
-				<IdeaCard {emoji} {name} {description} />
-			</li>
-		{/each}
-	</ul>
-	<p class="text-center">
-		<BigLink href="/ideas" prefetch={true}>See more!</BigLink>
-	</p>
-</section>
