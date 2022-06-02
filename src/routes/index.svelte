37a4acddbf945d57a7ae9e8e31d5d413b00d5214
Johnny Magrippis <j@magrippis.com>
2022-06-02 11:37:21 +0200

    ♻️  no need to nest inside another `main` element

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,21 +38,19 @@
 	<title>Techy Cat: Get inspired & do more tech!</title>
 </svelte:head>
 
-<main>
-	<Hero />
-	<section class="container mb-6 px-2 sm:px-0">
-		<h2 class="mb-4 text-2xl">Latest ideas</h2>
-		<ul
-			class="mb-6 flex max-w-prose flex-col gap-4 sm:grid sm:w-full sm:max-w-full sm:grid-cols-12"
-		>
-			{#each ideas as { emoji, name, description }}
-				<li class="sm:col-span-6 xl:col-span-4">
-					<IdeaCard {emoji} {name} {description} />
-				</li>
-			{/each}
-		</ul>
-		<p class="text-center">
-			<BigLink href="/ideas" prefetch={true}>See more!</BigLink>
-		</p>
-	</section>
-</main>
+<Hero />
+<section class="container mb-6 px-2 sm:px-0">
+	<h2 class="mb-4 text-2xl">Latest ideas</h2>
+	<ul
+		class="mb-6 flex max-w-prose flex-col gap-4 sm:grid sm:w-full sm:max-w-full sm:grid-cols-12"
+	>
+		{#each ideas as { emoji, name, description }}
+			<li class="sm:col-span-6 xl:col-span-4">
+				<IdeaCard {emoji} {name} {description} />
+			</li>
+		{/each}
+	</ul>
+	<p class="text-center">
+		<BigLink href="/ideas" prefetch={true}>See more!</BigLink>
+	</p>
+</section>
