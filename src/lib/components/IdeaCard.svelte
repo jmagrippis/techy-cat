c00a2bb465f5777480672aa19ed40c9ba702d6a2
Johnny Magrippis <j@magrippis.com>
2022-06-17 09:57:25 +0200

    💄  align cards better to account for long links

--- src/lib/components/IdeaCard.svelte
+++ src/lib/components/IdeaCard.svelte
@@ -4,10 +4,14 @@
 	export let description: string
 </script>
 
-<div class="flex items-center gap-4 rounded bg-surface-2 px-6 py-4 shadow">
-	<span class="text-6xl">{emoji}</span>
-	<div>
+<div
+	class="grid grid-cols-12 items-center gap-4 rounded bg-surface-2 px-6 py-4 shadow"
+>
+	<span class="col-span-3 text-center text-6xl sm:col-span-2">{emoji}</span>
+	<div class="col-span-9 sm:col-span-10">
 		<h2 class="text-2xl">{name}</h2>
-		{description}
+		<div class="break-words">
+			{description}
+		</div>
 	</div>
 </div>
