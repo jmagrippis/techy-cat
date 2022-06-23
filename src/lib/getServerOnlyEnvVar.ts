6db0352e23d114bf5ae3fda27c1ea8263d09af1e
Johnny Magrippis <j@magrippis.com>
2022-06-23 09:35:29 +0200

    ✨  add dotenv-flow again to import secret env vars during development

--- src/lib/getServerOnlyEnvVar.ts
+++ src/lib/getServerOnlyEnvVar.ts
@@ -1,6 +1,15 @@
-import {browser} from '$app/env'
+import {browser, dev} from '$app/env'
+
+let dotenvInitialised = false
 
 export const getServerOnlyEnvVar = (key: string) => {
+	if (dev && !dotenvInitialised) {
+		import('dotenv-flow').then(({config}) => {
+			config()
+			dotenvInitialised = true
+		})
+	}
+
 	if (browser) {
 		throw new Error('attempting to access server-only env var on the browser')
 	}
