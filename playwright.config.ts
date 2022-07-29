2b63efa9966685ea02e4cab744d80a24e267a090
Johnny Magrippis <j@magrippis.com>
2022-07-29 22:02:41 +0200

    ✅  test what an logged-in user should see!

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import {devices} from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
+	globalSetup: './src/setupPlaywright',
 	testDir: './tests',
 	/* Maximum time one test can run for. */
 	timeout: 30 * 1000,
