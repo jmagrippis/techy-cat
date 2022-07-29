36d856b414abb496eb39f4fdd0b588d810a64f50
Johnny Magrippis <j@magrippis.com>
2022-07-29 16:42:53 +0200

    💄  animate ideas in using motion-one

--- src/routes/ideas.svelte
+++ src/routes/ideas.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import IdeaCard from '$lib/components/IdeaCard.svelte'
 	import PageHeading from '$lib/components/PageHeading.svelte'
+	import {animate, stagger} from 'motion'
+	import {onMount} from 'svelte'
 
 	export let ideas: App.IdeaWithAuthorAndStarred[]
+
+	let listContainer: HTMLUListElement
+
+	onMount(() => {
+		if (!listContainer) return
+		const listItems = listContainer.querySelectorAll(':scope > li')
+		animate(
+			listItems,
+			{opacity: [0, 1], y: [100, 0]},
+			{duration: 1, delay: stagger(0.1)}
+		)
+	})
 </script>
 
 <svelte:head>
@@ -11,7 +25,7 @@
 
 <section class="container w-full grow px-2">
 	<PageHeading>Latest Ideas</PageHeading>
-	<ul class="flex max-w-prose flex-col gap-4">
+	<ul class="flex max-w-prose flex-col gap-4" bind:this={listContainer}>
 		{#each ideas as { id, emoji, name, description, starred }}
 			<li>
 				<IdeaCard {id} {emoji} {name} {description} {starred} />
