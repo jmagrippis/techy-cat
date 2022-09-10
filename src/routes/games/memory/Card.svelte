e95f4397d04124aa97eea67f88f38b2156e0acd8
Johnny Magrippis <j@magrippis.com>
2022-09-10 10:32:04 +0200

    💄  shake cards on missed guess

--- src/routes/games/memory/Card.svelte
+++ src/routes/games/memory/Card.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	export let face: string
 	export let revealed: boolean
+	export let shaking: boolean
 	export let handleClick: () => void
 </script>
 
 <button
 	class="relative col-span-3 flex h-24 w-16 select-none rounded ring-offset-transparent transition-transform duration-300 focus:ring-2 focus:ring-primary-400 focus:ring-offset-2 sm:h-56 sm:w-36 md:h-60 md:w-40 lg:h-64 lg:w-48"
 	class:revealed
+	class:shaking
 	on:click={handleClick}
 	disabled={revealed}
 >
@@ -22,6 +24,8 @@
 
 <style>
 	button {
+		--shake-amount: 10%;
+		--shake-duration: 0.3s;
 		transform-style: preserve-3d;
 	}
 
@@ -36,5 +40,41 @@
 
 	.front {
 		transform: rotateY(180deg);
+	}
+
+	.shaking .back {
+		animation: shake var(--shake-duration) ease-in-out infinite;
+	}
+
+	.shaking .front {
+		animation: shakeRotated var(--shake-duration) ease-in-out infinite;
+	}
+
+	@keyframes shake {
+		0%,
+		100% {
+			transform: translateX(0);
+		}
+		25%,
+		75% {
+			transform: translateX(var(--shake-amount));
+		}
+		50% {
+			transform: translateX(calc(var(--shake-amount) * -1));
+		}
+	}
+
+	@keyframes shakeRotated {
+		0%,
+		100% {
+			transform: translateX(0) rotateY(180deg);
+		}
+		25%,
+		75% {
+			transform: translateX(var(--shake-amount)) rotateY(180deg);
+		}
+		50% {
+			transform: translateX(calc(var(--shake-amount) * -1)) rotateY(180deg);
+		}
 	}
 </style>
