c08cb9e0692a81e76c834d99d9999d06f07a75a9
Johnny Magrippis <j@magrippis.com>
2022-09-30 18:30:06 +0200

    🔍  add custom meta tags for our leaderboard page

--- src/routes/games/memory/leaderboard/+page.server.ts
+++ src/routes/games/memory/leaderboard/+page.server.ts
@@ -1,11 +1,22 @@
 import type {Actions, PageServerLoad} from './$types'
 
-export const load: PageServerLoad = async ({locals}) => {
+export const load: PageServerLoad = async ({locals, url}) => {
 	const highScores: App.HighScore[] = await locals.highScoresRepo.getAll({
 		limit: 100,
 	})
 
-	return {highScores}
+	return {
+		highScores,
+		meta: {
+			title: 'Memory Game Leaderboard',
+			description:
+				'Check out the high scores of logged-in users playing the Techy Cat Memory Game 😸',
+			image: {
+				url: `${url.origin}/meta/game-memory.png`,
+				alt: 'Memory Game: A Techy Cat take on a classic!',
+			},
+		},
+	}
 }
 
 export const actions: Actions = {}
