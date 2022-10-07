de56defcaacf5ddbe409b081a0f39eeac3f55f65
Johnny Magrippis <j@magrippis.com>
2022-10-07 17:49:20 +0200

    🐛  may this somehow builds on CI

--- src/routes/meta/social-thumbnail/png/+server.ts
+++ src/routes/meta/social-thumbnail/png/+server.ts
@@ -1,17 +1,26 @@
-import {chromium} from 'playwright'
+import type {ChromiumBrowser} from 'playwright-core'
+import playwright from 'playwright-aws-lambda'
 
 import type {RequestHandler} from './$types'
 
 export const GET: RequestHandler = async ({url}) => {
-	const browser = await chromium.launch()
-	const page = await browser.newPage()
-	page.setViewportSize({width: 1200, height: 630})
-	const imageUrl = url.href.replace('/png', '')
-	await page.goto(imageUrl, {waitUntil: 'networkidle'})
-	const imageBuffer = await page.screenshot()
+	let browser: ChromiumBrowser | null = null
 
-	return new Response(imageBuffer, {
-		status: 200,
-		headers: new Headers({'Content-Type': 'image/png'}),
-	})
+	try {
+		browser = await playwright.launchChromium({headless: true})
+		const page = await browser.newPage()
+		page.setViewportSize({width: 1200, height: 630})
+		const imageUrl = url.href.replace('/png', '')
+		await page.goto(imageUrl, {waitUntil: 'networkidle'})
+		const imageBuffer = await page.screenshot()
+
+		return new Response(imageBuffer, {
+			status: 200,
+			headers: new Headers({'Content-Type': 'image/png'}),
+		})
+	} finally {
+		if (browser) {
+			await browser.close()
+		}
+	}
 }
