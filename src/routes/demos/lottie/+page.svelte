66443f814b8adc39ca5c1dff107deefcd9c5a2d3
Johnny Magrippis <j@magrippis.com>
2022-10-07 10:33:28 +0200

    ✨  add route for demos

---
+++ src/routes/demos/lottie/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import PageHeading from '$lib/components/PageHeading.svelte'
+</script>
+
+<section class="container w-full grow px-2">
+	<PageHeading>Animations with Lottie</PageHeading>
+
+	<p>[implement this before publishing the YouTube video 😅]</p>
+</section>
