1f8722f10e707fa93e1521c42651643d10d91eaf
Johnny Magrippis <j@magrippis.com>
2022-10-07 20:20:13 +0200

    🐛  Puppeteer + @sparticuz/chromium has a chance

--- src/routes/meta/social-thumbnail/png/+server.ts
+++ src/routes/meta/social-thumbnail/png/+server.ts
@@ -1,26 +1,22 @@
-import type {ChromiumBrowser} from 'playwright-core'
-import playwright from 'playwright-aws-lambda'
+import puppeteer from 'puppeteer'
+import chromium from '@sparticuz/chromium'
 
 import type {RequestHandler} from './$types'
 
 export const GET: RequestHandler = async ({url}) => {
-	let browser: ChromiumBrowser | null = null
+	const browser = await puppeteer.launch({
+		args: chromium.args,
+		headless: true,
+		executablePath: (await chromium.executablePath) ?? undefined,
+	})
+	const page = await browser.newPage()
+	page.setViewport({width: 1200, height: 630})
+	const imageUrl = url.href.replace('/png', '')
+	await page.goto(imageUrl, {waitUntil: 'networkidle0'})
+	const imageBuffer = await page.screenshot()
 
-	try {
-		browser = await playwright.launchChromium({headless: true})
-		const page = await browser.newPage()
-		page.setViewportSize({width: 1200, height: 630})
-		const imageUrl = url.href.replace('/png', '')
-		await page.goto(imageUrl, {waitUntil: 'networkidle'})
-		const imageBuffer = await page.screenshot()
-
-		return new Response(imageBuffer, {
-			status: 200,
-			headers: new Headers({'Content-Type': 'image/png'}),
-		})
-	} finally {
-		if (browser) {
-			await browser.close()
-		}
-	}
+	return new Response(imageBuffer, {
+		status: 200,
+		headers: new Headers({'Content-Type': 'image/png'}),
+	})
 }
