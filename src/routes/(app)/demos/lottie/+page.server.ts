625cdc34f1a387221005d447b70bbb409d4a5968
Johnny Magrippis <j@magrippis.com>
2022-10-07 20:20:13 +0200

    🐛  Puppeteer + @sparticuz/chromium has a chance

--- src/routes/(app)/demos/lottie/+page.server.ts
+++ src/routes/(app)/demos/lottie/+page.server.ts
@@ -1,9 +1,23 @@
 import type {PageServerLoad} from './$types'
 
-export const load: PageServerLoad = async () => ({
-	meta: {
-		title: 'Lottie + SvelteKit Interactive Demo',
-		description:
-			'A dedicated showcase of how to use Lottie for idle looping animations, as well as in response to user actions.',
-	},
-})
+export const load: PageServerLoad = async ({url}) => {
+	const imageUrl = new URL(`${url.origin}/meta/social-thumbnail/png`)
+	const title = 'Lottie + SvelteKit Interactive Demo'
+	const description =
+		'A dedicated showcase of how to use Lottie for idle looping animations, as well as in response to user actions.'
+
+	imageUrl.searchParams.set('title', title)
+	imageUrl.searchParams.set('subtitle', description)
+	imageUrl.searchParams.set('heartCount', '23')
+
+	return {
+		meta: {
+			title,
+			description,
+			image: {
+				url: imageUrl.href,
+				alt: 'Animations with Lottie & SvelteKit!',
+			},
+		},
+	}
+}
