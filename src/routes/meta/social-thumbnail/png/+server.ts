65fd9f014288d92f9961f2d423616e69781c1496
Johnny Magrippis <j@magrippis.com>
2022-10-11 21:07:41 +0200

    🔥  a few MB over the 50MB limit, we'll get you one day

--- src/routes/meta/social-thumbnail/png/+server.ts
+++ src/routes/meta/social-thumbnail/png/+server.ts
@@ -1,22 +1,6 @@
-import puppeteer from 'puppeteer'
-import chromium from '@sparticuz/chromium'
-
 import type {RequestHandler} from './$types'
 
-export const GET: RequestHandler = async ({url}) => {
-	const browser = await puppeteer.launch({
-		args: chromium.args,
-		headless: true,
-		executablePath: (await chromium.executablePath) ?? undefined,
+export const GET: RequestHandler = async () =>
+	new Response(null, {
+		status: 404,
 	})
-	const page = await browser.newPage()
-	page.setViewport({width: 1200, height: 630})
-	const imageUrl = url.href.replace('/png', '')
-	await page.goto(imageUrl, {waitUntil: 'networkidle0'})
-	const imageBuffer = await page.screenshot()
-
-	return new Response(imageBuffer, {
-		status: 200,
-		headers: new Headers({'Content-Type': 'image/png'}),
-	})
-}
