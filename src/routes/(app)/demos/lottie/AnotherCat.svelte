d81b182c5d3cc43937ca92eb572f689a55666be7
Johnny Magrippis <j@magrippis.com>
2022-10-11 21:45:54 +0200

    ✨  add another cat

---
+++ src/routes/(app)/demos/lottie/AnotherCat.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import {onMount} from 'svelte'
+	import lottie from 'lottie-web'
+
+	import animationData from './another-cat.json'
+
+	let container: HTMLElement
+
+	onMount(() => {
+		lottie.loadAnimation({
+			container,
+			animationData,
+		})
+	})
+</script>
+
+<div class="h-[50vh] animate-fade-in-once" bind:this={container} />
