6885bd2c910d087ede7df590b26610010a06aa3e
Johnny Magrippis <j@magrippis.com>
2022-11-04 15:44:55 +0100

    ✨  Demo a TypingAnimation component

--- src/routes/(app)/demos/typing-animation/+page.server.ts
+++ src/routes/(app)/demos/typing-animation/+page.server.ts
@@ -5,7 +5,18 @@ export const load: PageServerLoad = async ({url}) => {
 	const description =
 		'For a lightweight but still cool effect, with relatively little CSS & Javascript!'
 
+	const sentences = [
+		'SvelteKit tutorials',
+		'React guides',
+		'a good laugh!',
+		'a good time!!!',
+		'a great FRIDAY!',
+		'quirky livestreams',
+		'fluff-free videos',
+	]
+
 	return {
+		sentences,
 		meta: {
 			title,
 			description,
--- src/routes/(app)/demos/typing-animation/+page.svelte
+++ src/routes/(app)/demos/typing-animation/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import PageHeading from '$lib/components/PageHeading.svelte'
+	import TypingAnimation from './TypingAnimation.svelte'
+	import type {PageData} from './$types'
+
+	export let data: PageData
 </script>
 
 <section class="container w-full grow px-2">
 	<PageHeading>Svelte Typing Animation</PageHeading>
-	<div>Your go-to place for</div>
-	<div>a laugh</div>
+	<section class="text-4xl md:text-6xl">
+		<div class="text-primary-400">Your go-to place for</div>
+		<TypingAnimation sentences={data.sentences} />
+	</section>
 </section>
